0880f5299a9c5c27344b63fbd296f354d23184fa
AyyamAgauri <anishgauri00786@gmail.com>
2024-08-06 17:00:33 +0530

    updated exception file

--- src/exception.py
+++ src/exception.py
@@ -17,7 +17,7 @@ class CustomException(Exception):
     def __init__(self,error_message,error_details:sys):
 
         #Inheriting the super class output
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message= error_message_details(error_message, error_detail=error_details)
 
     def __str__(self):
