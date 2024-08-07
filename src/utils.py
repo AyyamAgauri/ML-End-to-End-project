c4b39bf0c45cde275beeb41bf0457644b6d7be7e
AyyamAgauri <anishgauri00786@gmail.com>
2024-08-07 23:41:08 +0530

    added function for evaluating models

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import os
 import sys
 import dill
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 #for the creation of pkl file
 def save_object(file_path, obj):
@@ -18,3 +19,29 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+#For Evaluating models trained in src/components/models.py
+
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            #Accessing every model present one by one
+            model = list(models.values())[i]
+            #Training model
+            model.fit(X_train,y_train)
+            #Train Output prediction
+            y_train_pred = model.predict(X_train)
+            #Test Output prediction
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+            return report
+    except Exception as e:
+        raise CustomException(e,sys)
